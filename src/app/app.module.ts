25ded42b6ee9c0063d426d10e275959d8cece67c
DuyJohannes <duy.hpk0870@sinhvien.hoasen.edu.vn>
2021-03-05 11:31:05 +0700

    ok

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './pages/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
