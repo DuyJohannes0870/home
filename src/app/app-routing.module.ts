71b9a140d2eb73f132ec5f0d9720718c840e9b41
LeeThanhBao <bao.lt5030@sinhvien.hoasen.edu.vn>
2021-03-06 11:31:33 +0700

    home fix cua Pao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, 
+  { path: '', loadChildren: () => import('./pages/home1/home1.module').then(m => m.Home1Module) }, 
   { path: 'sign-in', loadChildren: () => import('./pages/sign-in/sign-in.module').then(m => m.SignInModule) },
   { path: 'promo', loadChildren: () => import('./pages/promo/promo.module').then(m => m.PromoModule) },
   { path: 'home1', loadChildren: () => import('./pages/home1/home1.module').then(m => m.Home1Module) },
